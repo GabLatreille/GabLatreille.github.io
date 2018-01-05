bc95648a6d5ab6f217cbc9d3a6124840225d602a
Gabrielle Latreille <gabrielle.latreille@shopify.com>
2018-01-05 13:40:11 -0500

    Version 1.1 for web

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById('button1').onclick = switchGray;
-document.getElementById('button2').onclick = switchGray;
+document.getElementById('button1').onclick = switchTheme;
+document.getElementById('button2').onclick = switchTheme;
 
-function switchGray() {
+function switchTheme() {
   if (document.getElementsByTagName("div")[0].style.backgroundImage == 'url("img/lightpurple.png")') {
     document.getElementsByTagName("div")[0].style.backgroundImage = 'url("img/darkpurple.png")';
     document.getElementsByTagName("span")[0].style.backgroundImage = 'url("img/lightpurple.png")';
